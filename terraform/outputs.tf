a771d2209f074c749bcc05d56678df00ed10f004
Ben Meadmore <bmeadmore@sagacitysolutions.co.uk>
2025-06-06 12:22:58 +0100

    changing outputs and using private subnets

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,6 +9,12 @@ output "alb_zone_id" {
   value       = aws_lb.main.zone_id
 }
 
+# ECR Repository outputs
+output "ecr_repository_url" {
+  description = "URL of the ECR repository"
+  value       = aws_ecr_repository.app.repository_url
+}
+
 # ECS outputs
 output "ecs_cluster_name" {
   description = "Name of the ECS cluster"
@@ -39,7 +45,7 @@ output "vpc_id" {
 
 output "private_subnet_ids" {
   description = "IDs of the public subnets"
-  value       = data.aws_subnets.private.ids
+  value       = data.aws_subnets.all.ids
 }
 
 output "alb_security_group_id" {
